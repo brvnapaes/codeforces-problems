ee0f3a3954cd5e81990b510ff16b5c98ea052793
Bruna Paes <brunapaesms@gmail.com>
2024-01-15 15:08:41 -0300

    Solution to Next Round problem

---
+++ 800/next_round.py
@@ -0,0 +1,17 @@
+n, k = input().split(' ')
+n = int(n)
+k = int(k)
+
+scores = input().split(' ')
+advance = 0
+
+if 1 <= k <= n and n <= 50:
+    for score in scores:
+        try:
+            if int(score) > 0 and int(score) >= int(scores[k-1]):
+                advance += 1
+        except:
+            pass
+
+    print(advance)
+
