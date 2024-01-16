d693b2e60506da4cc3b4a209c0fd365fd347a90c
Bruna Paes <brunapaesms@gmail.com>
2024-01-16 17:12:29 -0300

    Solution to bit++ problem

---
+++ 800/bit++.py
@@ -0,0 +1,12 @@
+n = int(input())
+statement = ''
+result = 0
+
+for _ in range(n):
+    statement = input()
+    if "++" in statement:
+        result += 1
+    elif "--" in statement:
+        result -= 1
+
+print(result)
