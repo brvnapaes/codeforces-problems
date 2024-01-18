3113c50b90018b9f3961fbd7bdc1900fea0b13bf
Bruna Paes <brunapaesms@gmail.com>
2024-01-18 17:22:17 -0300

    Solution to Petya and Strings problem

---
+++ 800/petya_and_strings.py
@@ -0,0 +1,26 @@
+def main():
+    str1 = list(input().lower())
+    str2 = list(input().lower())
+
+    is_equal(str1, str2)
+
+def is_equal(str1, str2):
+
+    if str1 == str2:
+        print('0')
+        return
+    
+    length = len(str1)
+    
+    for i in range(length):
+        if str1[i] != str2[i]:
+            dif1 = str1[i]
+            dif2 = str2[i]
+            break
+
+    if dif1 > dif2:
+        print('1')
+    else:
+        print('-1')
+
+main()
