d83a3fd400d617faa8ad5fa9fa5b1b7fdfaa3012
Bruna Paes <brunapaesms@gmail.com>
2024-01-19 20:10:00 -0300

    Solutions to Helpful Maths and Word Capitalization problems

---
+++ 800/helpful_maths.py
@@ -0,0 +1,26 @@
+s = input().split('+')
+ones=[]
+twos=[]
+threes=[]
+sum = ''
+
+length = len(s)
+
+for num in s:
+    if num == '1':
+        ones.append(num)
+    elif num == '2':
+        twos.append(num)
+    elif num =='3':
+        threes.append(num)
+
+for num in ones:
+    sum += num + '+'
+
+for num in twos:
+    sum += num + '+'
+
+for num in threes:
+    sum += num + '+'
+
+print(sum[:-1])
---
+++ 800/word_capitalization.py
@@ -0,0 +1,6 @@
+string = input()
+
+first_letter = string[0].upper()
+
+print(first_letter, end='')
+print(string[1:])
