74d08951e2e0987e42e2aa139ef3afc3d2abb74d
Bruna Paes <brunapaesms@gmail.com>
2024-01-23 15:43:25 -0300

    Solution to Soldier and Bananas problem

---
+++ 800/soldier_and_bananas.py
@@ -0,0 +1,11 @@
+k, n, w = input().split(' ')
+total = 0
+borrow = 0
+
+for i in range(int(w)):
+    total += int(k) * (i + 1)
+
+if int(n) >= total:
+    print('0')
+else:
+    print(total - int(n))
