7145bf606cebd8197fa89b416d7a122e5302e1e8
Bruna Paes <brunapaesms@gmail.com>
2024-01-23 15:17:51 -0300

    Solution to Elephant problem

---
+++ 800/elephant.py
@@ -0,0 +1,20 @@
+import math
+
+def main():
+    coordinate = int(input())
+    print(int(steps(coordinate)))
+
+def steps(coordinate): 
+
+    if coordinate > 5:
+        if coordinate % 5 == 0:
+            return coordinate / 5
+        else:
+            return math.floor((coordinate / 5) + 1)
+    else:
+        return 1
+
+main()
+
+
+
